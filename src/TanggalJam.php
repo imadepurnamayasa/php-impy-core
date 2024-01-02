d1d96df41e192fb31223977f12e257197446f194
I Made Purnama Yasa <62276883+imadepurnamayasa@users.noreply.github.com>
2024-01-03 07:51:50 +0800

    update

---
+++ src/TanggalJam.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Imadepurnamayasa\PhpInti;
+
+class TanggalJam
+{
+    private $tanggalJam;
+
+    public function __construct($tanggalJamString = 'now')
+    {
+        $this->tanggalJam = new \DateTime($tanggalJamString);
+    }
+
+    public function sekarang()
+    {
+        return $this->tanggalJam->format('Y-m-d H:i:s');
+    }
+
+    public function tambahHari($hari)
+    {
+        $interval = new \DateInterval('P' . abs($hari) . 'D');
+        if ($hari < 0) {
+            $this->tanggalJam->sub($interval);
+        } else {
+            $this->tanggalJam->add($interval);
+        }
+    }
+
+    public function selisihHari($tanggalJamLain)
+    {
+        $tanggalJam = new \DateTime($tanggalJamLain);
+        $interval = $this->tanggalJam->diff($tanggalJam);
+        return $interval->format('%r%a');
+    }
+
+    public function selisihBulan($tanggalJamLain)
+    {
+        $tanggalJam = new \DateTime($tanggalJamLain);
+        $interval = $this->tanggalJam->diff($tanggalJam);
+        return $interval->format('%r%m');
+    }
+
+    public function selisihTahun($tanggalJamLain)
+    {
+        $tanggalJam = new \DateTime($tanggalJamLain);
+        $interval = $this->tanggalJam->diff($tanggalJam);
+        return $interval->format('%r%y');
+    }
+
+    public function selisihJam($tanggalJamLain)
+    {
+        $tanggalJam = new \DateTime($tanggalJamLain);
+        $interval = $this->tanggalJam->diff($tanggalJam);
+        return $interval->format('%r%H');
+    }
+
+    public function selisihMenit($tanggalJamLain)
+    {
+        $tanggalJam = new \DateTime($tanggalJamLain);
+        $interval = $this->tanggalJam->diff($tanggalJam);
+        return $interval->format('%r%i');
+    }
+
+    public function selisihDetik($tanggalJamLain)
+    {
+        $tanggalJam = new \DateTime($tanggalJamLain);
+        $interval = $this->tanggalJam->diff($tanggalJam);
+        return $interval->format('%r%S');
+    }
+}
---
+++ src/Terbilang.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Imadepurnamayasa\PhpInti;
+
+class Terbilang
+{
+    private $kata;
+
+    public function __construct()
+    {
+        $this->kata = array(
+            '0' => 'Nol',
+            '1' => 'Satu',
+            '2' => 'Dua',
+            '3' => 'Tiga',
+            '4' => 'Empat',
+            '5' => 'Lima',
+            '6' => 'Enam',
+            '7' => 'Tujuh',
+            '8' => 'Delapan',
+            '9' => 'Sembilan',
+            '10' => 'Sepuluh',
+            '11' => 'Sebelas',
+            '12' => 'Dua Belas',
+            '13' => 'Tiga Belas',
+            '14' => 'Empat Belas',
+            '15' => 'Lima Belas',
+            '16' => 'Enam Belas',
+            '17' => 'Tujuh Belas',
+            '18' => 'Delapan Belas',
+            '19' => 'Sembilan Belas',
+            '20' => 'Dua Puluh',
+            '30' => 'Tiga Puluh',
+            '40' => 'Empat Puluh',
+            '50' => 'Lima Puluh',
+            '60' => 'Enam Puluh',
+            '70' => 'Tujuh Puluh',
+            '80' => 'Delapan Puluh',
+            '90' => 'Sembilan Puluh'
+        );
+    }
+
+    public function konversi($angka)
+    {
+        if ($angka < 0 || $angka > 999999999999999) {
+            return 'Angka melewati batas (0 - 999999999999999)';
+        }
+
+        $hasil = '';
+
+        // Triliun
+        $triliun = floor($angka / 1000000000000);
+        if ($triliun > 0) {
+            $hasil .= $this->konversiKelompok($triliun) . ' Triliun ';
+            $angka %= 1000000000000;
+        }
+
+        // Miliar
+        $miliar = floor($angka / 1000000000);
+        if ($miliar > 0) {
+            $hasil .= $this->konversiKelompok($miliar) . ' Miliar ';
+            $angka %= 1000000000;
+        }
+
+        // Juta
+        $juta = floor($angka / 1000000);
+        if ($juta > 0) {
+            $hasil .= $this->konversiKelompok($juta) . ' Juta ';
+            $angka %= 1000000;
+        }
+
+        // Seribu
+        $seribu = floor($angka / 1000);
+        if ($seribu > 0) {
+            $hasil .= $this->konversiKelompok($seribu) . ' Seribu ';
+            $angka %= 1000;
+        }
+
+        // Seratus
+        $seratus = floor($angka / 100);
+        if ($seratus > 0) {
+            $hasil .= $this->kata[$seratus] . ' Seratus ';
+            $angka %= 100;
+        }
+
+        // Tens and ones
+        if ($angka > 0) {
+            if ($angka < 20) {
+                $hasil .= $this->kata[$angka];
+            } else {
+                $hasil .= $this->kata[floor($angka / 10) * 10] . ' ' . $this->kata[$angka % 10];
+            }
+        }
+
+        // Remove trailing space and return the result
+        return trim($hasil);
+    }
+
+    private function konversiKelompok($angka)
+    {
+        $hasil = '';
+
+        // Tens and ones
+        if ($angka > 0) {
+            if ($angka < 20) {
+                $hasil .= $this->kata[$angka];
+            } else {
+                $hasil .= $this->kata[floor($angka / 10) * 10] . ' ' . $this->kata[$angka % 10];
+            }
+        }
+
+        return $hasil;
+    }
+}

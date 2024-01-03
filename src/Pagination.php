be556f46199400d23b64107b56cd6a91b6ec2aa3
I Made Purnama Yasa <62276883+imadepurnamayasa@users.noreply.github.com>
2024-01-03 08:29:51 +0800

    update

--- src/Pagination.php
+++ src/Pagination.php
@@ -41,9 +41,19 @@ class Pagination
 
         $totalPages = $this->getTotalPages();
 
+        if ($this->currentPage > 3) {
+            $html .= '<li class="start"><a href="' . $this->baseUrl . '1">1</a></li>';
+            $html .= '<li class="dots">...</li>';
+        }
+
         for ($i = max(1, $this->currentPage - 2); $i <= min($this->currentPage + 2, $totalPages); $i++) {
             $class = ($i == $this->currentPage) ? 'currentpage' : 'page';
             $html .= '<li class="' . $class . '"><a href="' . $this->baseUrl . $i . '">' . $i . '</a></li>';
+        }
+
+        if ($this->currentPage + 2 < $totalPages) {
+            $html .= '<li class="dots">...</li>';
+            $html .= '<li class="end"><a href="' . $this->baseUrl . $totalPages . '">' . $totalPages . '</a></li>';
         }
 
         if ($this->currentPage < $totalPages) {

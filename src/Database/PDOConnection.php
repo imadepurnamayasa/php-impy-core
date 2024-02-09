aa7e4782594ced96cee4c7098f8772a0c4dd3a70
I Made Purnama Yasa <62276883+imadepurnamayasa@users.noreply.github.com>
2024-02-09 10:55:33 +0800

    update

---
+++ src/Database/PDOConnection.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Imadepurnamayasa\PhpInti\Database;
+
+use PDO;
+use PDOException;
+
+abstract class PDOConnection
+{
+    protected $pdo;
+
+    public function __construct(string $dsn, string $username, string $password, array $options = [])
+    {
+        try {
+            $this->pdo = new PDO($dsn, $username, $password, $options);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $this->pdo;
+        } catch (PDOException $e) {
+            return json_encode([
+                'messages' => $e->getMessage()
+            ]);
+        }
+    }
+
+    public function close()
+    {
+        $this->pdo = null;
+    }
+}
---
+++ src/Database/PdoMysql_.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Imadepurnamayasa\PhpInti\Database;
+
+class PdoMysql extends PDOConnection
+{
+    public function __construct($host, $dbname, $username, $password, $options = [])
+    {
+        $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
+        parent::__construct($dsn, $username, $password, $options);
+    }
+}

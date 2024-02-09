6154985771c1ddc2e3c2d2ed2cd6b4ffcb0815aa
I Made Purnama Yasa <62276883+imadepurnamayasa@users.noreply.github.com>
2024-02-09 09:55:28 +0800

    update

--- src/Crud/Constants.php
+++ src/Crud/Constants.php
@@ -4,10 +4,10 @@ namespace Imadepurnamayasa\PhpInti\Crud;
 
 class Constants
 {
-    const DATA_TYPE_STRING = 0;
-    const DATA_TYPE_NUMBER = 1;
-    const DATA_TYPE_DECIMAL = 2;
-    const DATA_TYPE_DATE = 3;
-    const DATA_TYPE_TIME = 4;
-    const DATA_TYPE_DATETIME = 5;
+    const DATA_TYPE_STRING = 'string';
+    const DATA_TYPE_NUMBER = 'number';
+    const DATA_TYPE_DECIMAL = 'decimal';
+    const DATA_TYPE_DATE = 'date';
+    const DATA_TYPE_TIME = 'time';
+    const DATA_TYPE_DATETIME = 'datetime';
 }
